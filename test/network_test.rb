63cf49647d9a38d369d7e9b187b3476903d294a7
Andrew Reid <areid.sj@gmail.com>
2020-02-11 09:33:08 -0700

    Write and pass it adds shows test

--- test/network_test.rb
+++ test/network_test.rb
@@ -7,6 +7,12 @@ require './lib/network'
 class Network_Test < Minitest::Test
   def setup
     @nbc = Network.new("NBC")
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
   end
 
   def test_it_exists
@@ -19,5 +25,12 @@ class Network_Test < Minitest::Test
 
   def test_it_begins_with_no_shows
     assert_equal [], @nbc.shows
+  end
+
+  def test_it_adds_shows
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
   end
 end
